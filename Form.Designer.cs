dcb4992b68e518a4938bce0dd06f2c326de1d57e
Samjackrep <maxenen@gmail.com>
2013-09-24 21:57:23 +0800

    Initial Commit [Program Still WIP]
    Initial commit for the iPod Mod Sound Importer. Only works for Windows
    :P
    
    I'm still figuring out some stuff, too.

---
+++ Form.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PlaybackModUtil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.lstSounds = new System.Windows.Forms.ListBox();
+            this.btnConv = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.importDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(12, 148);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(89, 32);
+            this.btnImport.TabIndex = 1;
+            this.btnImport.Text = "Import...";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // lstSounds
+            // 
+            this.lstSounds.FormattingEnabled = true;
+            this.lstSounds.Location = new System.Drawing.Point(12, 12);
+            this.lstSounds.Name = "lstSounds";
+            this.lstSounds.Size = new System.Drawing.Size(441, 134);
+            this.lstSounds.TabIndex = 3;
+            this.lstSounds.SelectedIndexChanged += new System.EventHandler(this.lstSounds_SelectedIndexChanged);
+            // 
+            // btnConv
+            // 
+            this.btnConv.Location = new System.Drawing.Point(206, 148);
+            this.btnConv.Name = "btnConv";
+            this.btnConv.Size = new System.Drawing.Size(247, 32);
+            this.btnConv.TabIndex = 4;
+            this.btnConv.Text = "Save and Confirm";
+            this.btnConv.UseVisualStyleBackColor = true;
+            this.btnConv.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 186);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(441, 25);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // importDialog
+            // 
+            this.importDialog.Filter = "MP3 files|*.mp3|WAV files|*.wav";
+            this.importDialog.Multiselect = true;
+            this.importDialog.Title = "Import";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Remove";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 222);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstSounds);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnConv);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Playback Mod Utility";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.ListBox lstSounds;
+        private System.Windows.Forms.Button btnConv;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.OpenFileDialog importDialog;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Core;
+using System.IO.Compression;
+using System.Threading;
+
+namespace PlaybackModUtil
+{
+    public partial class Form1 : Form
+    {
+        ArrayList sounds = new ArrayList();
+        String modsDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft\mods";
+        String zipDir = ""; // This will be set in Form1()
+        ZipFile zip;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Find the mod's zip directory
+            bool isFound = false;
+            foreach (String fN in Directory.GetFiles(modsDir))
+            {
+                if (fN.Substring(fN.LastIndexOf('\\') + 1, 10) == "iPodProMod")
+                {
+                    zipDir = fN;
+                    isFound = true;
+                    break;
+                }
+            }
+            if (isFound == false)
+            {
+                MessageBox.Show("iPod Mod wasn't found at " + modsDir, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // THIS.CLOSE()!!!!!!!!!!!!!!
+            }
+            zip = new ZipFile(zipDir);
+            ListAllSounds();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            ArrayList obj = new ArrayList();
+            DialogResult result = importDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                foreach (String fileName in importDialog.FileNames)
+                {
+                    obj.Add(fileName);
+                }
+                ImportSound(obj);
+            }
+        }
+
+        private void lstSounds_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Still Buggy (Last thing to fix!!)
+            // Just found out that you need java (de)compiler stuff
+            // You cannot find any file such as OggSounds.java in the mod zip
+
+            String tempDir = modsDir + @"\TEMP_23";
+            String javaFile = Path.Combine(tempDir, "OggSounds.java");
+            if (!Directory.Exists(tempDir)) Directory.CreateDirectory(tempDir);
+            FileStream fs = new FileStream(javaFile, FileMode.Create, FileAccess.ReadWrite);
+
+            foreach (ZipEntry zipEntry in zip)
+            {
+                if (!zipEntry.IsFile) continue;
+                if (zipEntry.Name == "org/TeamL33T/IpodMod/OggSounds.java")
+                {
+                    zip.GetInputStream(zipEntry).CopyTo(fs);
+                    break;
+                }
+            }
+
+            StreamReader reader = new StreamReader(fs);
+            String content = reader.ReadToEnd();
+            content = content.Substring(content.LastIndexOf(';') + 1);
+            String endContent = "";
+            ArrayList newLines = new ArrayList();
+            ArrayList codeLines = new ArrayList();
+
+            foreach (String ogg in lstSounds.Items)
+            {
+                newLines.Add(ogg);
+            }
+
+            foreach (String line in newLines)
+            {
+                endContent = "\nevent.manager.addSound(\"ipodmod:" + line + "\");";
+                codeLines.Add(endContent);
+            }
+
+            foreach (String codeLine in codeLines)
+            {
+                content += codeLine;
+            }
+
+            content += "\n{
+        }
+
+
+        private void ListAllSounds()
+        {
+            lstSounds.Items.Clear();
+            foreach (ZipEntry zipEntry in zip)
+            {
+                if (zipEntry.Name.Length <= 20) continue;
+                if (zipEntry.Name.Substring(0, 21) == "assets/ipodmod/sound/")
+                {
+                    if (!zipEntry.IsFile) continue; // Ignore directories
+                    lstSounds.Items.Add(zipEntry.Name.Substring(21));
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (lstSounds.SelectedIndex < 0)
+            {
+                MessageBox.Show("Please pick a sound to remove", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            } else {
+                RemoveSound(lstSounds.SelectedItem.ToString());
+            }
+            ListAllSounds();
+        }
+
+        private void ImportSound(ArrayList oggPaths)
+        {
+            zip.BeginUpdate();
+            foreach (String oggPath in oggPaths)
+            {
+                String oggName = oggPath.Substring(oggPath.LastIndexOf('\\') + 1);
+                zip.Add(oggPath, "assets/ipodmod/sound/" + oggName);
+            }
+            zip.CommitUpdate();
+            ListAllSounds();
+        }
+
+        private void RemoveSound(string oggName)
+        {
+            zip.BeginUpdate();
+            foreach (ZipEntry zipEntry in zip)
+            {
+                if (zipEntry.Name == "assets/ipodmod/sound/" + oggName)
+                {
+                    zip.Delete(zipEntry.Name);
+                    break;
+                }
+            }
+            zip.CommitUpdate();
+            ListAllSounds();
+        }
+
+    }
+}
